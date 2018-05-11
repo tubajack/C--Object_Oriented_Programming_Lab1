e9d6a9d5e3fc698cd992043f0ba368b6e6c729af
Jackson Henry <jackson.henry13@yahoo.com>
2018-05-11 13:50:01 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedCSharp1
+{
+  
+    public class Car
+    {
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        private static int instances = 0;
+
+        public Car(string Color, int year)
+        {
+            this.Color = Color;
+            this.Year = year;
+            instances++;
+        }
+
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            instances++;
+        }
+
+        public Car()
+        {
+             instances++;
+        }
+
+        public static int CountCars()
+        {
+            return instances;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedCSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate a new car- Car1
+            var Car1 = new Car();
+
+            //Use dot notation to call members of Car1
+            Car1.Color = "green";
+            Car1.Year = 2002;
+            Car1.Mileage = 200000;
+
+            //Instantiate a second car
+            var Car2 = new Car("red", 2012);
+
+            //Access static members
+            int CarCount = Car.CountCars();
+
+            // Output to the console window
+            Console.WriteLine($"There are {CarCount} cars on inventory right now.");
+
+        }
+    }
+}
